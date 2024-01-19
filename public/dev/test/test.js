17a8505791d11569cd285ac40dccfdbba1fc72e2
kellyzen <02tankelly@gmail.com>
2024-01-20 00:16:02 +0800

    Added modal popup

--- public/dev/test/test.js
+++ public/dev/test/test.js
@@ -1,6 +1,12 @@
-document.getElementById('timer').innerHTML =  05 + ":" + 00;
-startTimer();
+// Modal popup
+document.getElementById('modal-start').classList.add('open');
 
+function startTest() {
+    document.getElementById('modal-start').classList.remove('open');
+    startTimer();
+}
+
+// Countdown Timer
 function startTimer() {
     var presentTime = document.getElementById('timer').innerHTML;
     var timeArray = presentTime.split(/[:]+/);
@@ -8,12 +14,12 @@ function startTimer() {
     var s = checkSecond((timeArray[1] - 1));
     if (s == 59) { m = m - 1 }
     if (m < 0) {
+        // document.getElementById('modal-end').classList.add('open');
         return
     }
 
     document.getElementById('timer').innerHTML =
         m + ":" + s;
-    console.log(m)
     setTimeout(startTimer, 1000);
 
 }
